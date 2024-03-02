58a0242b5ccb7b5db04cdffa150447ed9b8cb396
Sheetal Pujari <pujari.sh@northeastern.edu>
2024-03-02 11:18:57 -0500

    added workflow

--- integration.test.js
+++
@@ -1,9 +0,0 @@
-import get from 'axios';
-import {expect} from 'chai';
-
-describe('/health endpoint integration tests', () => {
-    it ('should return a 200 status code healthcheck', async()=>{
-        const response = await get('http://localhost:3000/api/health');
-        expect(response.status).to.equal(200);
-    });
-});
