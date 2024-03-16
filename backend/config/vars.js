6fa2df33c4f5372b5eb37e2c7d0246032f3a0eea
Sheetal Pujari <pujari.sh@northeastern.edu>
2024-03-16 11:27:21 -0400

    env file removed

--- backend/config/vars.js
+++ backend/config/vars.js
@@ -2,10 +2,10 @@ const path = require('path');
 
 const appPackage = require('../package.json');
 
-require('dotenv-safe').config({
-  path: path.join(__dirname, '../.env'),
-  sample: path.join(__dirname, '../.env.example'),
-});
+// require('dotenv-safe').config({
+//   path: path.join(__dirname, '../.env'),
+//   sample: path.join(__dirname, '../.env.example'),
+// });
 
 module.exports = {
   appName: appPackage.name,
