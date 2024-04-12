9e285c13ba616248b14f03b36494bf65e51f5f7b
Rajavi Shah <shah.raja@northeastern.edu>
2024-04-12 00:38:34 -0400

    About UI Enhancement

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -8,36 +8,43 @@ import '../css/About.css';
 const cardData = [
   {
     title: 'Inspiration',
-    content: `It was amidst the bustling streets of New York City and the serene shores of New Jersey that the concept of TravelWing took flight. As an international student embarking on this trip, our founder encountered the very challenges many travelers face: the daunting task of creating an itinerary that aligns perfectly with one's unique preferences and schedule. While "Top 10 Things to Do" lists and standard tour packages offered a glimpse into possible experiences, they fell short of the personalized journey desired. Each suggested plan was a one-size-fits-all solution that didn't fit at all. Moreover, the booking process felt like piecing together a complex puzzle with scattered pieces across different websites.`
+    content: `It was amidst the bustling streets of New York City and the serene shores of New Jersey that the concept of TravelWing took flight. As an international student embarking on this trip, our one of the team-members encountered the very challenges many travelers face: the daunting task of creating an itinerary that aligns perfectly with one's unique preferences and schedule. While "Top 10 Things to Do" lists and standard tour packages offered a glimpse into possible experiences, they fell short of the personalized journey desired. Each suggested plan was a one-size-fits-all solution that didn't fit at all. Moreover, the booking process felt like piecing together a complex puzzle with scattered pieces across different websites.`
   },
   {
     title: 'Concept',
-    content: `TravelWing emerged as a beacon for travelers yearning for customization. Leveraging the prowess of AI and the versatility of the OpenAI API, we present you with the ability to craft a travel plan that's as unique as your fingerprints. TravelWing isn't just a tool; it's your personal travel artisan that weaves your desires, dates, and dreams into a coherent, comprehensive itinerary.`
+    content: `TravelWing emerged as a beacon for travelers yearning for customization. Leveraging the power of the OpenAI API, we present you with the ability to craft a travel plan that's as unique as your fingerprints. TravelWing goes beyond being a mere tool; it's your personal travel guide, seamlessly blending your preferences, dates, and aspirations into a cohesive and thorough itinerary.`
   },
   
 ];
 
 const secondCard = [
   {
-    title: 'Our Mission: Redefining Travel Experiences',
-    content: `At TravelWing, we're on a mission to redefine the way you travel. We believe that every journey should be a reflection of your individuality, not a checklist of tourist traps. Our commitment to you is to provide a seamless, stress-free experience that puts the joy back into travel planning.`
+    title: 'Itinerary Planning',
+    content: `With TravelWing, planning your itinerary is as simple as filling out a prompt. Tell us what you seek, and our AI will retrieve information to curate a travel plan that resonates with your wishes. For our members, these plans aren't just ephemeral; save them for future reference or have them sent to you in a neatly packaged PDF via email.` 
   },
   {
-    title: 'Our Vision: A World of Possibilities',
-    content: `Our vision is to create a world where travel isn't just a destination; it's a transformative experience. We envision a future where every traveler can embark on a journey that's uniquely theirs, filled with unforgettable moments and cherished memories. With TravelWing, the world is your oyster, and the possibilities are endless.`
+    title: 'Flight Booking',
+    content: `Input your preferences, and our integration with the Amadeus API provides you with up-to-date flight details. When you're ready to take to the skies, a seamless transition to Skyscanner awaits to secure your booking.`
   },
   {
-    title: 'Our Values: Innovation, Integrity, Inclusivity',
-    content: `Innovation is at the heart of everything we do at TravelWing. We're constantly pushing the boundaries of what's possible to bring you the best travel experience. Integrity is our guiding principle, and we're committed to transparency, honesty, and accountability in all our interactions. Inclusivity is our promise to you; we welcome travelers from all walks of life and strive to create a safe, welcoming space for everyone.`
+    title: 'Hotel Reservations',
+    content: `Find your home away from home by entering your lodging preferences. TravelWing partners with renowned platforms to ensure that the hotel you book is not just a stay but an experience to cherish.`
   },
 ];
 
+const commitmentCard = [
+  {
+    title: 'Personalization',
+    content: `TravelWing is more than a platform; it's a promise — a promise to deliver a travel planning and booking experience that is as delightful as it is efficient. It's about transforming the oft-tedious pre-travel arrangements into an exhilarating prelude to your adventure. With TravelWing, your travel planning becomes an integral part of the adventure — a prelude filled with anticipation and excitement.`
+  }
+];
+
 const teamMembers = [
-  { name: 'Sheetal', role: 'Product Manager', img: 'flight1.png' },
-  { name: 'Vishal', role: 'Product Manager', img: 'flight1.png' },
-  { name: 'Rajavi', role: 'Product Manager', img: 'flight1.png' },
-  { name: 'Sanath', role: 'Product Manager', img: 'flight1.png' },
-  { name: 'Sravanti', role: 'Product Manager', img: 'flight1.png' },
+  { name: 'Vishal', role: 'Software Developer', img: 'Vishal.png' },
+  { name: 'Sheetal', role: 'Software Developer', img: 'Sheetal.png' },
+  { name: 'Sravanti', role: 'Software Developer', img: 'Sravanti.png' },
+  { name: 'Rajavi', role: 'Software Developer', img: 'Rajavi.png' },
+  { name: 'Sanath', role: 'Software Developer', img: 'Sanath.png' },
   
 ];
 
@@ -63,7 +70,7 @@ const AboutPage = () => {
 
       <Box className="card-container">
         {cardData.map((card, index) => (
-          <motion.div className="card" key={index} whileHover={{ scale: 1.05 }}>
+          <motion.div className="main-card" key={index} whileHover={{ scale: 1.05 }}>
             <Typography className="card-title">{card.title}</Typography>
             <Typography className="card-text">{card.content}</Typography>
           </motion.div>
@@ -79,6 +86,20 @@ const AboutPage = () => {
       <Box className="second-card-container">
         {secondCard.map((card, index) => (
           <motion.div className="card" key={index} whileHover={{ scale: 1.05 }}>
+            <Typography className="card-title">{card.title}</Typography>
+            <Typography className="card-text">{card.content}</Typography>
+          </motion.div>
+        ))}
+      </Box>
+
+      {/* Adding a title for the Our Commitment section */}
+      <Box className="section-title">
+        <Typography variant="h4" style={{ textAlign: 'center', margin: '20px 0' }}>Our Commitment</Typography>
+      </Box>
+
+      <Box className="commitment-card-container">
+        {commitmentCard.map((card, index) => (
+          <motion.div className="commitment-card" key={index} whileHover={{ scale: 1.05 }}>
             <Typography className="card-title">{card.title}</Typography>
             <Typography className="card-text">{card.content}</Typography>
           </motion.div>
@@ -102,10 +123,7 @@ const AboutPage = () => {
         ))}
       </Box>
 
-      {/* Commitment Section */}
-      <Box className="commitment-section">
-        {/* Commitment text */}
-      </Box>
+      
 
       <Box className="typewriter-heading">
         <Typewriter
