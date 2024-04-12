b754410ba33088c7ac47bc867cd14baac67a7f8d
Vishal Raj <vishalraj1802@gmail.com>
2024-04-11 20:24:27 -0400

    adds redirects from landing page to react client

--- lp/app/(home)/components/Navbar.tsx
+++ lp/app/(home)/components/Navbar.tsx
@@ -1,9 +1,15 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Navbar(){
     return (
         <nav className="py-10 flex justify-between items-center">
             <h1 className="text-2xl font-bold underline underline-offset-8 decoration-green-500">TravelWing ✈️</h1>
+            {/* <Link 
+            <h1 className="text-2xl font-bold underline underline-offset-8 decoration-green-500">Explore</h1> */}
+            <Link href="http://localhost:3000">
+                <h1 className="text-2xl font-bold underline underline-offset-8 decoration-green-500">Explore</h1>
+            </Link>
         </nav>
     );
 }
